7d0e33d18f468ac7fe3cf49be56684c0494e4b83
Joe Raven <joakim.ringstad.raavi@gmail.com>
2023-05-20 13:33:32 +0200

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
-
-
-
+  //var link = "https://cdn.coverr.co/videos/coverr-robot-vacuum-cleaner-8823/1080p.mp4"
+  //var video = document.getElementById("myVideo");
+  //video.setAttribute("src", link);
 });
 
 
@@ -20,5 +20,58 @@ function setVisible(selector, visible) {
 
 onReady(function() {
   $("#loading").fadeOut(500);
+
     $(".page").fadeIn(500);
+    setTimeout(lightson,1000); 
+    setTimeout(showheader,2000);
+     
 });
+
+function lightson(){
+  $(".headerbg").css({
+    transition: '0.8s linear',
+    filter: 'brightness(0.8)'
+});
+}
+function showheader(){
+  $(".headerbgTextContent").fadeIn(500);
+}
+
+
+window.addEventListener("scroll", function() {
+
+  const maxHeight = document.body.scrollHeight - window.innerHeight;
+  const percentage = ((window.pageYOffset * 100) / maxHeight);
+  var a = 10;
+  var b = 80;
+  console.log(percentage);
+  var pos = $(window).scrollTop();
+  pos = percentage
+  console.log(pos);
+  if((pos > a) && (pos < b)) {
+    
+    //document.getElementsByTagName(".headerbg").style.transition = "2s";
+    //document.getElementsByTagName(".headerbg").style.filter = "brightness(0.4)";
+
+      //This function was removed 2023-05-13 due to dark bg image jr
+        /*$(".headerbg").css({
+          transition: '0.4s linear',
+          filter: 'brightness(0.2)'
+      });*/
+  }
+  else if((pos>b)) {
+    $(".headerbg").css({
+      filter: 'brightness(0.5)'
+  });
+  }
+  else {
+      $(".headerbg").css({
+                  filter: 'brightness(0.8)'
+              });
+  }
+});
+
+$(window).scroll(function(){ 
+
+
+  });
