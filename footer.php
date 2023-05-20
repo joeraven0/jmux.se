b990283d8166a78c84fe7792eb26060666a2d8a6
Joe Raven <joakim.ringstad.raavi@gmail.com>
2023-05-20 13:56:03 +0200

    Add files via upload
    feature folder & images

--- footer.php
+++ footer.php
@@ -1,16 +1,16 @@
 <footer class="page">
 
-<div class="flexi">
-  <div class="potrait">
-    <img src="assets/potrait.jpg" alt="Bild på mig">
-    <h3>Hej hej!</h3>
+  <div class="flexi">
+    <div class="potrait">
+      <img src="assets/potrait.jpg" alt="Bild på mig">
+      <h3>Hej hej!</h3>
+    </div>
+    <div class="contact">
+      <p>JMUX Elektronik & data<br>Joakim Ringstad<br>0790-222881<br>joakim@jmux.se</p>
+      <p>Postadress: Bergmästarevägen 8D<br>691 52 Karlskoga (endast post, ej besöksadress)</p>
+      <p>Godkänd för F-skatt<br>jmux.se::V0.4</p>
+    </div>
   </div>
-  <div class="contact">
-    <p>JMUX Mjukvara & datahantering<br>Joakim Ringstad<br>0790-222881<br>joakim@jmux.se</p>
-    <p>Postadress: Bergmästarevägen 8D, 691 52 Karlskoga (endast post, ej besöksadress)<br>
-    Godkänd för F-skatt</p>
-  </div>
-</div>
 </footer>
 </body>
 <link rel="stylesheet" type="text/css" href="css/normalize.css">
@@ -18,8 +18,13 @@
 <link rel="stylesheet" type="text/css" href="css/header.css">
 <link rel="stylesheet" type="text/css" href="css/nav.css">
 <link rel="stylesheet" type="text/css" href="css/main.css">
+
 <link rel="stylesheet" type="text/css" href="css/footer.css">
 <link rel="icon" href="../assets/logo.ico">
+
+
+<link rel="stylesheet" type="text/css" href="features/customer_stories/assets/css/style.css">
+<script type="text/javascript" src="features/customer_stories/assets/js/javascript.js"></script>
 
 
 <script type="text/javascript" src=" https://code.jquery.com/jquery-1.12.4.min.js"></script>
@@ -35,6 +40,7 @@
 
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet"> 
+<link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+
 </html>
